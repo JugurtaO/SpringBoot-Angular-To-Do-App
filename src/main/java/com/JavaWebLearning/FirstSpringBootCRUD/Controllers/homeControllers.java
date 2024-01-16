22db7083226e81054304c7d5dd02a1d8e3bc768c
JugurtaO <jugurtaourzik@gmail.com>
2024-01-16 22:34:58 +0100

    +master: signout controller and home controllers overall done

--- src/main/java/com/JavaWebLearning/FirstSpringBootCRUD/Controllers/homeControllers.java
+++ src/main/java/com/JavaWebLearning/FirstSpringBootCRUD/Controllers/homeControllers.java
@@ -2,7 +2,6 @@ package com.JavaWebLearning.FirstSpringBootCRUD.Controllers;
 
 import com.JavaWebLearning.FirstSpringBootCRUD.Services.HomeServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ public class homeControllers {
         this.homeServices = homeServices;
     }
 
-    @GetMapping("")
+    @RequestMapping (value = {"","/home"})
 public String welcome(){
     return homeServices.welcome();
 }
