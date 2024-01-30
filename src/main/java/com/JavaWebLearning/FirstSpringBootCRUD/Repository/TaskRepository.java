c1fd1d2be6b53f1418a6bc2a66165647f93b2a3f
JugurtaO <jugurtaourzik@gmail.com>
2024-01-30 08:34:41 +0100

    +Master: Task controllers overall done

--- src/main/java/com/JavaWebLearning/FirstSpringBootCRUD/Repository/TaskRepository.java
+++ src/main/java/com/JavaWebLearning/FirstSpringBootCRUD/Repository/TaskRepository.java
@@ -1,11 +1,18 @@
 package com.JavaWebLearning.FirstSpringBootCRUD.Repository;
 
 import com.JavaWebLearning.FirstSpringBootCRUD.Models.Task;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository <Task,Integer>{
+    @Transactional
+    @Modifying
+    @Query(value="SELECT * FROM task WHERE author_id = ?1",nativeQuery = true)
     public Optional<Task[]> findTasksByAuthorId(int author_id);
+
 
 }
